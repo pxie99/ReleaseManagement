4cef49d4440f3573a6dfd9261e22b8c0b8ed8b92
yuanzh <yuanzh@cisco.com>
2017-08-07 13:55:53 +0800

    a script to help branch checkout in packaing-pe

---
+++ bin/checkout-pe.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash 
+#set -x
+
+##############################################################
+# Function to checkout PE submodules to a specified branch
+##############################################################
+
+ws=`pwd`
+git_params="$@"
+
+REPOS=(
+ "3rdParty/externalLibs"
+ "data-plane-common"
+ "Packaging"
+ "traffic-server"
+ "unittest-content"
+ "."
+)
+
+ORGANIZATION="Interstellar"
+
+for item in ${REPOS[@]}
+do
+	cd $ws
+	if [ -d $item ]; then
+		cd $item
+		git checkout $git_params
+	else
+		echo "submodule $item doesn't exist! "
+		exit 1
+	fi
+done
+
+
